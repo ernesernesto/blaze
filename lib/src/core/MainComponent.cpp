7ffb41d02ee2bc83b01c02079e8740da0079dcbc
ernesernesto <ernesernesto@gmail.com>
2014-06-11 17:00:30 +0700

    initial commit

---
+++ lib/src/core/MainComponent.cpp
@@ -0,0 +1,97 @@
+#include <Windows.h>
+#include <stdint.h>
+#include <thread>
+
+#include "MainComponent.h"
+
+using namespace Blaze;
+
+MainComponent::MainComponent(Window* window)
+	: _window(window)
+	, _isRunning(false)
+{
+}
+
+MainComponent::~MainComponent()
+{
+}
+
+void MainComponent::Start()
+{
+	run();
+}
+
+void MainComponent::Stop()
+{
+	_isRunning = false;
+}
+
+void MainComponent::run()
+{
+	_isRunning = true;
+	int frames = 0;
+	double frameCounter = 0;
+
+	const double frameTime = 1.0 / 5000;
+
+	double previousTime = timeGetTime();
+	double lag = 0.0;
+
+	while(_isRunning)
+	{
+		bool needRender = false;
+
+		double currentTime = timeGetTime();
+		double passedTime = currentTime - previousTime;
+		previousTime = currentTime;
+
+		lag += passedTime / 1000;
+		frameCounter += passedTime / 1000;
+
+		while (lag >= frameTime)
+		{
+			needRender = true;
+			lag -= frameTime;
+			
+			if(!_window->IsCloseRequested())
+				Stop();
+
+			//TODO UPDATE GAME
+			//game.input
+			//game.update()
+
+
+			if(frameCounter >= 1)
+			{
+				printf("%d\n", frames);
+				frames = 0;
+				frameCounter = 0;
+			}
+		}
+
+		if(needRender)
+		{
+			frames++;
+			render();
+			//TODO RENDER PER FRAMETIME
+		}
+		else
+		{
+			std::chrono::milliseconds duration(1);
+			std::this_thread::sleep_for(duration);
+		}
+	}
+
+	cleanUp();
+}
+
+void MainComponent::render()
+{
+	//game.render
+	_window->Render();
+}
+
+void MainComponent::cleanUp()
+{
+	delete _window;
+}
